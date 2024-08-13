2d561028dc38f400ee17fd7cd4556465e656a15a
Karla <karlamel248@gmail.com>
2024-08-12 18:12:37 -0600

    Solo aceptar carácteres permitidos

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const texto = document.querySelector('.text-area');
 const textoEncriptado = document.getElementById("texto-encriptado");
-//const mensaje = document.querySelector("mensaje-contenedor-texto");
 const botonCopiar = document.getElementById("copy");
 const mensajeContenedorTexto = document.querySelector('.mensaje-contenedor-texto');
 const imagen = document.querySelector('.mensaje-contenedor-imagen');
@@ -15,52 +14,69 @@ const contenedorSubtitulo = document.querySelector('.mensaje-contenedor-subtitul
 
 botonCopiar.style.display = 'none';
 
+function validarTexto(input) {
+    const valido = /^[a-z\s]*$/; // Solo letras minúsculas y espacios
+    return valido.test(input);
+}
+
 function encriptar(str_encriptada) {
-    const matriz_codigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
-    str_encriptada = str_encriptada.toLowerCase();
+    if (!validarTexto(str_encriptada)) {
+        alert("El texto contiene caracteres no permitidos. Por favor, usa solo letras minúsculas y sin acentos.");
 
-    for (let i = 0; i < matriz_codigo.length; i++) {
-        if (str_encriptada.includes(matriz_codigo[i][0])) {
-            str_encriptada = str_encriptada.replaceAll(matriz_codigo[i][0], matriz_codigo[i][1]);
-        }
     }
+    else{
+        const matriz_codigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+        str_encriptada = str_encriptada.normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, '') // Eliminar acentos
+            .replace(/[^a-z\s]/g, ''); // Eliminar caracteres especiales y números
 
-    return str_encriptada;
+        for (let i = 0; i < matriz_codigo.length; i++) {
+            if (str_encriptada.includes(matriz_codigo[i][0])) {
+                str_encriptada = str_encriptada.replaceAll(matriz_codigo[i][0], matriz_codigo[i][1]);
+            }
+        }
+        botonCopiar.style.display = 'block';
+        mensajeContenedorTexto.style.display = 'block';
+        imagen.style.display = 'none';
+        contenedorTitulo.style.display = 'none';
+        contenedorSubtitulo.style.display = 'none';
+        return str_encriptada;
+    }
 }
 
 function btn_encriptar() {
     const texto_encriptado = encriptar(texto.value);
     textoEncriptado.textContent = texto_encriptado;
-    botonCopiar.style.display = 'block';
-    mensajeContenedorTexto.style.display = 'block';
-    imagen.style.display = 'none';
-    contenedorTitulo.style.display = 'none';
-    contenedorSubtitulo.style.display = 'none';
 }
 
 
 function desencriptar(str_desencriptada){
-    const matriz_codigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
-    str_desencriptada = str_desencriptada.toLowerCase();
+    if (!validarTexto(str_desencriptada)) {
+        alert("El texto contiene caracteres no permitidos. Por favor, usa solo letras minúsculas y sin acentos.");
 
-    for (let i =0; i<matriz_codigo.length; i++){
-        if (str_desencriptada.includes(matriz_codigo[i][1])){
-            str_desencriptada = str_desencriptada.replaceAll(matriz_codigo[i][1], matriz_codigo[i][0]);
-        }
     }
-    return str_desencriptada;
+    else {
+        const matriz_codigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+
+        for (let i =0; i<matriz_codigo.length; i++){
+            if (str_desencriptada.includes(matriz_codigo[i][1])){
+                str_desencriptada = str_desencriptada.replaceAll(matriz_codigo[i][1], matriz_codigo[i][0]);
+            }
+        }
+        botonCopiar.style.display = 'block';
+        mensajeContenedorTexto.style.display = 'block';
+        imagen.style.display = 'none';
+        contenedorTitulo.style.display = 'none';
+        contenedorSubtitulo.style.display = 'none';
+        return str_desencriptada;
+}
 }
 
 function btn_desencriptar() {
     const texto_desencriptado = desencriptar(texto.value);
     textoEncriptado.textContent = texto_desencriptado;
-    botonCopiar.style.display = 'block';
-    mensajeContenedorTexto.style.display = 'block';
-    imagen.style.display = 'none';
-    contenedorTitulo.style.display = 'none';
-    contenedorSubtitulo.style.display = 'none';
+    
 }
-
 
 function btn_copiar(){
     navigator.clipboard.writeText(textoEncriptado.textContent).then(()=>{
